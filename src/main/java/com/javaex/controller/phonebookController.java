742adbe507ca8c2539c74fd471d642bba372f125
jiny0852 <jiny0852@gmail.com>
2024-08-19 18:02:20 +0900

    2024-08-19

--- src/main/java/com/javaex/controller/phonebookController.java
+++ src/main/java/com/javaex/controller/phonebookController.java
@@ -1,8 +1,13 @@
 package com.javaex.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javaex.vo.PersonVo;
 
 @Controller
 public class phonebookController {
@@ -19,14 +24,7 @@ public class phonebookController {
 	//리스트
 	
 	//등록폼
-	@RequestMapping ( value="/writeform", method={RequestMethod.GET, RequestMethod.POST}  )
-	public String writeForm() {
-		
-		System.out.println("phonebookController.writeForm()");
-		
-		return "/WEB-INF/views/writeForm.jsp";
-		
-	}
+
 	
 	@RequestMapping ( value="/list", method={RequestMethod.GET, RequestMethod.POST}  )
 	public String list() {
@@ -34,6 +32,15 @@ public class phonebookController {
 		System.out.println("phonebookController.list()");
 		
 		return "/WEB-INF/views/list.jsp";
+		
+	}
+	
+	@RequestMapping ( value="/writeform", method={RequestMethod.GET, RequestMethod.POST}  )
+	public String writeForm() {
+		
+		System.out.println("phonebookController.writeForm()");
+		
+		return "/WEB-INF/views/writeForm.jsp";
 		
 	}
 	
@@ -45,6 +52,36 @@ public class phonebookController {
 		return "/WEB-INF/views/editForm.jsp";
 		
 	}
+	
+	@RequestMapping ( value="/insert", method={RequestMethod.GET, RequestMethod.POST}  )
+	public String insertPerson ( @ModelAttribute PersonVo personVo ) {
+		
+		PersonVo pVo = personVo;
+		
+		
+		return "redirect:/views/list.jsp";
+	}
+	
+	
+	@RequestMapping ( value="/update/{no}", method={RequestMethod.GET, RequestMethod.POST}  )
+	public String updatePerson ( @PathVariable("no") int no ) {
+		
+		int id = no;
+		
+				
+		
+		return "redirect:/views/list.jsp";
+	}
+	
+	@RequestMapping ( value="/delete", method={RequestMethod.GET, RequestMethod.POST}  )
+	public String deletePerson ( @RequestParam(value="personid") int id ) {
+		
+		
+		
+		
+		return "redirect:/views/list.jsp";
+	}
+	
 	
 
 	
